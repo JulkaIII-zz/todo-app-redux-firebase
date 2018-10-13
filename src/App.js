5d67c77f8baf9e8f7c601aa64ecd9549a0fafc5f
Yuliia Shtohryn <julia.ilkiv@gmail.com>
2018-10-14 02:15:21 +0300

    added components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
+import Dashboard from "./components/dashboard/Dashboard";
+import Navbar from "./components/layout/Navbar";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
+          <Navbar />
           <Switch>
             <Route path="/" component={Dashboard} />
           </Switch>
---
+++ src/components/layout/SignedInLinks.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const SignedInLinkes = () => {
+  return (
+    <ul className="right">
+      <li>
+        <NavLink to="/">New Note</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Logout</NavLink>
+      </li>
+      <li>
+        <NavLink to="/" className="btn btn-floating ping lighten-1">
+          JS
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default SignedInLinkes;
