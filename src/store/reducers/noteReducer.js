84fedadca27e9df876d5abeef5684980a1fca0ef
Yuliia Shtohryn <julia.ilkiv@gmail.com>
2018-10-15 01:06:26 +0300

    added connection between dashboard and store

--- src/store/reducers/noteReducer.js
+++ src/store/reducers/noteReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+  notes: [
+    { id: "1", title: "note 1", content: "lalalala" },
+    { id: "2", title: "note 1", content: "lalalala" },
+    { id: "3", title: "note 1", content: "lalalala" }
+  ]
+};
 
 const noteReducer = (state = initState, action) => {
   return state;
