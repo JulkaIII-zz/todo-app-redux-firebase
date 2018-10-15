f1b27806a72cf6998987036484514d2e5d799f23
Yuliia Shtohryn <julia.ilkiv@gmail.com>
2018-10-15 23:52:37 +0300

    added create note request for firebase DB

--- src/store/actions/noteActions.js
+++ src/store/actions/noteActions.js
@@ -1,10 +1,23 @@
 export const createNote = note => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // dispatch - dispatching an action to reducer
-    // here make async call to DB .post()
-    dispatch({
-      type: "CREATE_NOTE",
-      note
-    });
+    const firestore = getFirestore();
+    firestore
+      .collection("notes")
+      .add({
+        ...note,
+        authorFirstName: "Jonny",
+        authorLastName: "Grey",
+        createdAt: new Date()
+      })
+      .then(() => {
+        dispatch({
+          type: "CREATE_NOTE",
+          note
+        });
+      })
+      .catch(err => {
+        dispatch({ type: "CREATE_NOTE_ERROR", err });
+      });
   };
 };
